4c578a4be95f271dc85652ab9e6c17c70b451de8
Drake Smith <drakesmithjr@gmail.com>
2017-10-27 19:21:57 -0700

    add API key to .gitignore

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,13 +2,15 @@ import React, { Component } from 'react';
 import logo from '../../logo.svg';
 import styles from './App.scss';
 
+import API_KEY from '../../config-key.js'
+
 class App extends Component {
   render() {
     return (
       <div className={styles.app}>
         <header className={styles.header}>
           <img src={logo} className={styles.logo} alt="logo" />
-          <h1 className="App-title">Welcome to React with SCSS and CSS Modules</h1>
+          <h1 className="App-title">Art Scramble</h1>
         </header>
         <p className={styles.intro}>
           This App.js file's styles comes from importing App.scss. Using CSS Modules, the styles are scoped locally to this App component.
