c90f8420e6140645970029111a47a6d529e62124
Drake Smith <drakesmithjr@gmail.com>
2017-10-28 17:57:59 -0700

    grid and img heights and widths working

--- src/components/Tile/Tile.js
+++ src/components/Tile/Tile.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import styles from './Tile.scss';
 
-const Tile = (props) => {
-  return (
-    <div style={{'float': 'left', 'display': 'inlineBlock'}}>
-      <img style={{'margin': '5px'}} src={props.url} onClick={props.onClick} />
-    </div>
-  )
-};
+class Tile extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.handleImgResize = this.handleImgResize.bind(this)
+  }
+
+  handleImgResize() {      
+  }
+  //<div style={{'float': 'left', 'display': 'inlineBlock'}}>
+  //    </div>
+  render() {
+    return (
+      <img className={styles.tileImg} src={this.props.url} onClick={this.props.onClick} onLoad={this.handleImgResize}
+      />
+    )
+  }
+}
 
 export default Tile;
